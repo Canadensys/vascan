ad04891af8415cfab288b897b34089488a041f89
Christian Gendreau <christiangendreau@gmail.com>
2015-02-03 09:25:50 -0500

    This is NOT the chosen solution for Issue #23 Commit for future reference or documentation

--- src/main/java/net/canadensys/dataportal/vascan/controller/PagesController.java
+++ src/main/java/net/canadensys/dataportal/vascan/controller/PagesController.java
@@ -6,20 +6,37 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import net.canadensys.dataportal.vascan.config.VascanConfig;
+import net.canadensys.exception.web.ResourceNotFoundException;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PagesController {
-	//private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd");
+	
+	/**
+	 * Handle SiteMesh calls for decorators.
+	 * 
+	 * @param request
+	 * @param decoratorName
+	 * @return
+	 */
+	@RequestMapping("/decorators/{decoratorName}")
+	public ModelAndView handleDecorator(HttpServletRequest request, @PathVariable String decoratorName){
+
+		// Do not respond to direct request, only internal forward
+		if(request.getAttribute("javax.servlet.forward.request_uri") == null){
+			throw new ResourceNotFoundException();
+		}
+		return new ModelAndView(decoratorName);
+	}
 
 	@RequestMapping(value={"/about"}, method={RequestMethod.GET})
 	public ModelAndView handleAboutPage(HttpServletRequest request){
 		Map<String,Object> model = new HashMap<String,Object>();
-		//model.put("currentDate", SDF.format(Calendar.getInstance().getTime()));
 		ControllerHelper.addOtherLanguageUri(request, model);
 		return new ModelAndView("about", VascanConfig.PAGE_ROOT_MODEL_KEY, model);
 	}
@@ -27,7 +44,6 @@ public class PagesController {
 	@RequestMapping(value={"/api"}, method={RequestMethod.GET})
 	public ModelAndView handleApiPage(HttpServletRequest request){
 		Map<String,Object> model = new HashMap<String,Object>();
-		//model.put("currentDate", SDF.format(Calendar.getInstance().getTime()));
 		
 		ControllerHelper.addOtherLanguageUri(request, model);
 		return new ModelAndView("api", VascanConfig.PAGE_ROOT_MODEL_KEY, model);
